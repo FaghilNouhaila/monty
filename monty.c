f3d7624abc48b51c98e44bd7876f73f7b974da67
Faghil Nouhaila <Faghilnouhaila@gmail.com>
2023-06-21 06:49:32 -0700

    File monty

---
+++ monty.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+global_var var_global;
+/**
+ * main - driver function for monty program
+ * @ac: int num of arguments
+ * @ay: opcode file
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	stack_t *stack;
+
+	stack = NULL;
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n";
+		exit(EXIT_FAILURE);
+	}
+
+	read_file(ay[1], &stack);
+	/* recordar liberar memories */
+	free_dlistint(stack);
+	return (0);
+}
--- monty.h
+++ monty.h
@@ -3,16 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+
+/*--- Struct Definitions ---*/
+extern int push_arg;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -25,9 +29,8 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -35,4 +38,52 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct global_variable - opcode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct global_variable
+{
+	FILE *file;
+	int push_arg;
+	char *buffer;
+} global_var;
+
+extern global_var var_global;
+
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack,unsigned int line_number);
+instruct_func get_op_func(char *str);
+
+/*Functions Monty */
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/*Funnctions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/*Functions math */
+void _sub(stack_t **stack,unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
+/*Utility functions */
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
 #endif /* __MONTY_H__ */
